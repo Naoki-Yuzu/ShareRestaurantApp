3312a8d29e81d1375f7b68ba1a8e728f9624d5b6
Naoki-Yuzu <toaru.programming@gmail.com>
2020-02-13 15:44:15 +0900

    initial setting
    add some codes to make screen without  storyboard

--- ShareRestaurantApp/AppDelegate.swift
+++ ShareRestaurantApp/AppDelegate.swift
@@ -12,11 +12,21 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        if #available(iOS 13, *) {
+        } else {
+
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+            
+        }
+        
         return true
     }
 
