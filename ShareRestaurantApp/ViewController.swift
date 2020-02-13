a6fb69d912500ae6efc23804fbb268cb0b12a3b0
Naoki-Yuzu <toaru.programming@gmail.com>
2020-02-13 15:19:47 +0900

    initial commit

---
+++ ShareRestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareRestaurantApp
+//
+//  Created by デュフフくん on 2020/02/13.
+//  Copyright © 2020 Naoki-Yuzu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
